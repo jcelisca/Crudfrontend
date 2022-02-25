67826e5f7e56d4e2af21b420b51443e31a549faa
jcelisca <celiss1997@gmail.com>
2022-02-24 20:49:22 -0500

    Usereducer para el renderizado de estados

--- src/App.js
+++ src/App.js
@@ -15,6 +15,50 @@ const Store = createContext(initialState)
 const StoreProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+
+  /**
+   * Reducer que hace el renderizado de los componentes segun el estado
+   * @param {*} state Estado del componente segun el contexto
+   * @param {*} action Acción para el estado actual
+   * @returns Nuevo estado
+   */
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'update-item':
+        const todoUpItem = state.todo;
+        const listUpdateEdit = todoUpItem.list.map((item) => {
+          if (item.id === action.item.id) {
+            return action.item;
+          }
+          return item;
+        });
+        todoUpItem.list = listUpdateEdit;
+        todoUpItem.item = {};
+        return { ...state, todo: todoUpItem }
+      case 'delete-item':
+        const todoUpDelete = state.todo;
+        const listUpdate = todoUpDelete.list.filter((item) => {
+          return item.id !== action.id;
+        });
+        todoUpDelete.list = listUpdate;
+        return { ...state, todo: todoUpDelete }
+      case 'update-list':
+        const todoUpList = state.todo;
+        todoUpList.list = action.list;
+        return { ...state, todo: todoUpList }
+      case 'edit-item':
+        const todoUpEdit = state.todo;
+        todoUpEdit.item = action.item;
+        return { ...state, todo: todoUpEdit }
+      case 'add-item':
+        const todoUp = state.todo.list;
+        todoUp.push(action.item);
+        return { ...state, todo: {list: todoUp, item: {}} }
+      default:
+        return state;
+    }
+  }
+
   return <Store.Provider value={{ state, dispatch }}>
     {children}
   </Store.Provider>
@@ -23,10 +67,11 @@ const StoreProvider = ({ children }) => {
 
 
 function App() {
-  return (
+  return <StoreProvider>
     <h3>To-Do List</h3>
-  );
-
+    <Form />
+  </StoreProvider>
 }
+
 
 export default App;
